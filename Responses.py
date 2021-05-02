d50c05c66fb74ce6decb92c93ee8eb2070755b79
Lorenzo Cavallero <61491088+lorenzocavallero@users.noreply.github.com>
2021-05-02 14:55:07 +0200

    Update Responses.py

--- Responses.py
+++ Responses.py
@@ -1,6 +1,6 @@
 from datetime import datetime 
 
-def sample_responsed(input_text):
+def sample_responses(input_text):
   user_message = str(input_text).lower()
   
   if user_message in ("hello", "hi", "sup"):
