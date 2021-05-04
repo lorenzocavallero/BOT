72f804bfe5698433556efb83b8ccff8f4108bf6d
lorenzocavallero <lorenzo.cavallero91@gmail.com>
2021-05-04 16:26:39 +0200

    Responses.py

--- Responses.py
+++ Responses.py
@@ -3,7 +3,7 @@ from datetime import datetime
 def sample_responses(input_text):
   user_message = str(input_text).lower()
   
-  if user_message in ("Hello", "Hi", "Hello!"):
+  if user_message in ("hello", "hi", "hello!"):
     return "Welcome"
   if user_message in ("who are you", "who are you?"):
     return "I'm a Bot!"
