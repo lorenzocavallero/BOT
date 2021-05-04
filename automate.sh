56e90466d0b69398e4e31cc29a8422d8743e0000
lorenzocavallero <lorenzo.cavallero91@gmail.com>
2021-05-04 18:13:50 +0200

    automate.sh

--- automate.sh
+++ automate.sh
@@ -1,16 +1,1 @@
-cd /your-path-to-git/project
-
-git checkout -b branch-name
-git remote update
-
-UPSTREAM=${1:-'@{u}'}
-LOCAL=$(git rev-parse @)
-REMOTE=$(git rev-parse "$UPSTREAM")
-BASE=$(git merge-base @ "$UPSTREAM")
-
-if [ $LOCAL = $REMOTE ]; then
-    echo "Up-to-date"
-elif [ $LOCAL = $BASE ]; then
-    echo "Need to pull"
-    sh pull_code.sh     
-fi
+git pull
